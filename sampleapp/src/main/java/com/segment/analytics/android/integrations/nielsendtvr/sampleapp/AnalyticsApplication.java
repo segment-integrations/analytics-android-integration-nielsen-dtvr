c2043c5e6012fa7b9e5a8fa41581c6ebc86ca784
Michael Chen <michael.chen@ensemble.com>
2019-06-06 18:15:06 -0700

    sample app first implementation

---
+++ sampleapp/src/main/java/com/segment/analytics/android/integrations/nielsendtvr/sampleapp/AnalyticsApplication.java
@@ -0,0 +1,29 @@
+package com.segment.analytics.android.integrations.nielsendtvr.sampleapp;
+
+import android.app.Application;
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.android.integrations.nielsendtvr.NielsenDTVRIntegration;
+
+public class AnalyticsApplication extends Application {
+    private final static String WRITE_KEY = BuildConfig.WRITE_KEY;
+    private AnalyticsController analyticsController;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initAnalytics();
+    }
+
+    private void initAnalytics() {
+        analyticsController = new AnalyticsController(
+                new Analytics.Builder(this, WRITE_KEY)
+                        .logLevel(Analytics.LogLevel.VERBOSE)
+                        .use(NielsenDTVRIntegration.FACTORY)
+                        .build());
+    }
+
+    public AnalyticsController getAnalytics() {
+        return analyticsController;
+    }
+}
--- sampleapp/src/main/java/com/segment/analytics/android/integrations/nielsendtvr/sampleapp/MainActivity.java
+++ sampleapp/src/main/java/com/segment/analytics/android/integrations/nielsendtvr/sampleapp/MainActivity.java
@@ -1,15 +1,22 @@
 package com.segment.analytics.android.integrations.nielsendtvr.sampleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import com.segment.analytics.android.integrations.nielsendtvr.NielsenDTVRIntegration;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.mainButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), VideoActivity.class));
+            }
+        });
     }
 }
