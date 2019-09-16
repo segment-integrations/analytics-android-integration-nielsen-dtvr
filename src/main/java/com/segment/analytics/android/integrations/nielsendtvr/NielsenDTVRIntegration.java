d1c8fd4e08321c269a3191f43cbe49d5033f0552
Brennan Gamwell <brennan@segment.com>
2019-09-16 11:19:27 -0700

    Update loadType mapping.

--- src/main/java/com/segment/analytics/android/integrations/nielsendtvr/NielsenDTVRIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/nielsendtvr/NielsenDTVRIntegration.java
@@ -89,8 +89,15 @@ public class NielsenDTVRIntegration extends Integration<AppSdk> {
       if (properties.containsKey("channel"))
         jsonMetadata.put("channelName", properties.getString("channel"));
 
+      String load_type = "";
       if (properties.containsKey("load_type"))
-        jsonMetadata.put("adModel", properties.getString("load_type").equals("linear") ? "1" : "2");
+        load_type = "load_type";
+
+      if (properties.containsKey("loadType"))
+        load_type = "loadType";
+
+      if (!load_type.isEmpty())
+        jsonMetadata.put("adModel", properties.getString(load_type).equals("dynamic") ? "2" : "1");
 
     } catch (JSONException e) {
       logger.error(e, "Failed to send loadMetadata event");
